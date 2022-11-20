bfe684711be6937eb9eeded03317c20842b7ca76
kkk125504 <kginyc5504@gmail.com>
2022-11-20 16:50:59 +0900

    환경 세팅

---
+++ src/main/java/com/kjh/exam/demo/SbAm1Application.java
@@ -0,0 +1,13 @@
+package com.kjh.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAm1Application.class, args);
+	}
+
+}
