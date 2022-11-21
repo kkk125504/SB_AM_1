63809912ce4faf3a5f5eae6ca1c081bc240d79b9
kkk125504 <kginyc5504@gmail.com>
2022-11-21 22:48:16 +0900

    메인페이지 추가

--- src/main/java/com/kjh/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/kjh/exam/demo/controller/UsrHomeController.java
@@ -1,18 +1,7 @@
 package com.kjh.exam.demo.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.kjh.exam.demo.service.ArticleService;
-import com.kjh.exam.demo.util.Ut;
-import com.kjh.exam.demo.vo.Article;
-import com.kjh.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrHomeController {
