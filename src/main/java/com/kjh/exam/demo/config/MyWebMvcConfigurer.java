9aee6a96df1046a2fc281ecffab2db03d7506b52
kkk125504 <kginyc5504@gmail.com>
2022-11-27 01:32:07 +0900

    마이페이지(View)

--- src/main/java/com/kjh/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/kjh/exam/demo/config/MyWebMvcConfigurer.java
@@ -23,6 +23,7 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
 		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
 				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
-				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete")
+				.addPathPatterns("/usr/member/myPage");;
 	}
 }
