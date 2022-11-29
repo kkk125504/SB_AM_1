4f608db1abc442bdd0bb7e4e29407e34e045f05b
kkk125504 <kginyc5504@gmail.com>
2022-11-29 15:59:39 +0900

    afterLoginUri가 없을시 메인페이지로 이동, 로그아웃시 원래페이지로 이동(로그인을 필요로 한다면 메인페이지 이동)

--- src/main/java/com/kjh/exam/demo/vo/Rq.java
+++ src/main/java/com/kjh/exam/demo/vo/Rq.java
@@ -127,9 +127,23 @@ public class Rq {
 		case "/usr/member/join":
 		case "/usr/member/findLoginId":
 		case "/usr/member/findLoginPw":
-			return Ut.getUriEncoded(paramMap.get("afterLoginUri"));
+			return Ut.getUriEncoded(Ut.getAttr(paramMap, "afterLoginUri", ""));
 		}
 		return getEncodedCurrentUri();
 	}
-
+	
+	public String getLogoutUri() {
+		return "../member/doLogout?afterLogoutUri=" + getAfterLogoutUri();
+	}
+	
+	public String getAfterLogoutUri() {
+		String requestUri = req.getRequestURI();
+		switch (requestUri) {
+		case "/usr/article/write":
+		case "/usr/article/modify":
+		case "/usr/member/myPage":
+			return Ut.getUriEncoded("/");
+		}
+		return getEncodedCurrentUri();
+	}
 }
